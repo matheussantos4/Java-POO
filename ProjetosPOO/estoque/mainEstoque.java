bad4a62cca60a0c384f31c52edd293f19b03eb39
matheussantos4 <matheus.santos88y@gmail.com>
2026-04-14 15:44:14 -0300

    add mainEstoque and produto classes for inventory management

---
+++ ProjetosPOO/estoque/mainEstoque.java
@@ -0,0 +1,12 @@
+package ProjetosPOO.estoque;
+
+import java.util.Scanner;
+
+public class mainEstoque {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+}
---
+++ ProjetosPOO/estoque/produto.java
@@ -0,0 +1,9 @@
+package ProjetosPOO.estoque;
+
+public class produto {
+
+    String nome;
+    Double price;
+    int quantidade = 0;
+
+}
