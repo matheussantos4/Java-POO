11c3ea4478c1030634d9e8db9cdee93a52b4399f
matheussantos4 <matheus.santos88y@gmail.com>
2026-04-22 19:23:51 -0300

    init calculator

---
+++ topicosPOO/MembrosEstaticos/Calculator.java
@@ -0,0 +1,14 @@
+package topicosPOO.MembrosEstaticos;
+
+public class Calculator {
+
+    public static final double PI = 3.14159;
+
+    public double circumference(double radius) {
+        return 2.0 * PI * radius;
+    }
+
+    public double volume(double radius) {
+        return 4.0 * PI * radius * radius * radius / 3;
+    }
+}
---
+++ topicosPOO/MembrosEstaticos/raioCirculo.java
@@ -0,0 +1,27 @@
+package topicosPOO.MembrosEstaticos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class raioCirculo {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Calculator cal1 = new Calculator();
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double c = cal1.circumference(radius);
+        double v = cal1.volume(radius);
+
+        System.out.printf("Circumference:  %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI value:  %.2f%n", cal1.PI);
+
+        sc.close();
+    }
+}
