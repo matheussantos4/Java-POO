4a808a20d9148b3d7eaea9643d64d651cb28d334
matheussantos4 <matheus.santos88y@gmail.com>
2026-05-18 19:22:42 -0300

    doc: descrição de funcionalidades.

--- EncapsulamentoConstrutores/MediaAlunos/ex2Main.java
+++ EncapsulamentoConstrutores/MediaAlunos/ex2Main.java
@@ -7,8 +7,10 @@ public class ex2Main {
 
         Scanner sc = new Scanner(System.in);
 
+        //criação de turma a partir da classe turmaAlunos
         turmaAlunos turma1 = new turmaAlunos("JavaPOO");
 
+        //criação de aluno a partir da classe Aluno
         Aluno a1 = new Aluno("Matheus", 8.4);
         turma1.adicionarAluno(a1);
 
@@ -16,12 +18,22 @@ public class ex2Main {
         System.out.println("Status: Aluno " + a1.getNome() + " adicionado com sucesso a turma " + turma1.getNomeTurma());
         System.out.println("Matricula do aluno: " + a1.getMatricula());
         System.out.println("Nota final: " + a1.getNota());
+
+        
         System.out.println("Total de alunos na turma: " + turma1.getQuantidadeAlunos());
 
+        //adição de alunos na respectiva classe criada a partir
+        //da classe turmaAlunos
+
         Aluno a2 = new Aluno("Maria", 9.0);
-        turma1.adicionarAluno(a1);
+        turma1.adicionarAluno(a2);
         Aluno a3 = new Aluno("João", 7.0);
-        turma1.adicionarAluno(a1);
+        turma1.adicionarAluno(a3);
+
+        /*metodo acima utiliza do adicionarAluno, função que soma
+        alunos dentro de uma turma e ao mesmo tempo realiza a
+        contagem de sua matricula (décimo terceiro aluno =
+        13 matricula.*/
 
         System.out.println("Total de alunos na turma: " + turma1.getQuantidadeAlunos());
 
