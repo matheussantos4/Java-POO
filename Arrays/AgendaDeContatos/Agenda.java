6399d0c6d4b20542f5afa5a01b0864740845fc70
matheussantos4 <matheus.santos88y@gmail.com>
2026-05-19 20:36:24 -0300

    feat:Desenvolvimento classe Agenda

--- Arrays/AgendaDeContatos/Agenda.java
+++ Arrays/AgendaDeContatos/Agenda.java
@@ -1,4 +1,27 @@
 package Arrays.AgendaDeContatos;
 
 public class Agenda {
+
+    private String nomeAgenda;
+    private int totalContatos;
+
+    private Contato[] listaContatos = new Contato[50];
+
+    public String getNomeAgenda() {
+        return nomeAgenda;
+    }
+
+    public void setNomeAgenda(String nomeAgenda) {
+        if (nomeAgenda == null || nomeAgenda.isBlank()) {
+
+        } else this.nomeAgenda = nomeAgenda;
+    }
+
+    public int getTotalContatos() {
+        return totalContatos;
+    }
+
+    public void setTotalContatos(int totalContatos) {
+        this.totalContatos = totalContatos;
+    }
 }
