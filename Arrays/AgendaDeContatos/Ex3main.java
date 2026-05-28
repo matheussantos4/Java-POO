41865a1edf723bd3cd684f33a7df0a11f2eee2d5
matheussantos4 <matheus.santos88y@gmail.com>
2026-05-28 19:38:04 -0300

    feat: desenvolvimento de introdução dos dados.

--- Arrays/AgendaDeContatos/Ex3main.java
+++ Arrays/AgendaDeContatos/Ex3main.java
@@ -7,6 +7,22 @@ public class Ex3main {
 
         Scanner sc = new Scanner(System.in);
 
+        Agenda MinhaAgenda = new Agenda();
+
+        Contato c1 = new Contato("Bruno", "11999999999", "brunoburghi@gmail.com");
+
+        do {
+            System.out.println("Nome: ");
+            c1.getNome();
+
+        } while (c1.getNome() == null);
+
+
+        MinhaAgenda.adicionarContato(c1);
+
+        System.out.println(c1.getEmail());
+        System.out.println();
+
     }
 }
  
