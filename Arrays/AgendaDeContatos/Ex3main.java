12c06ea4f702d398dab39edefb3dd95b90275267
matheussantos4 <matheus.santos88y@gmail.com>
2026-05-28 20:44:53 -0300

    feat: Interface interativa para adição de contatos.

--- Arrays/AgendaDeContatos/Ex3main.java
+++ Arrays/AgendaDeContatos/Ex3main.java
@@ -12,6 +12,22 @@ public class Ex3main {
         Contato c1 = new Contato("Bruno", "11999999999", "brunoburghi@gmail.com");
 
         do {
+            System.out.println("Adicionar contato(1) Sair(2)");
+            opcao = sc.nextInt();
+            if (opcao == 1) {
+                sc.nextLine();
+
+                Contato c = new Contato(sc.nextLine(), sc.nextLine(), sc.nextLine());
+                MinhaAgenda.adicionarContato(c);
+                System.out.println("Contato adicionado!");
+            } else System.out.print("Saindo...");
+
+        } while (opcao == 1);
+
+        System.out.println();
+
+
+        /*do {
             System.out.println("Nome: ");
             c1.getNome();
 
