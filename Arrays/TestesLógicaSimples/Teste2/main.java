5bd4491cf93d759815bf76cea78b51f06e4efe8c
matheussantos4 <matheus.santos88y@gmail.com>
2026-07-09 19:44:32 -0300

    feat:testes loop array + for.

--- Arrays/TestesLógicaSimples/Teste2/main.java
+++ Arrays/TestesLógicaSimples/Teste2/main.java
@@ -1,4 +1,15 @@
 package Arrays.TestesLógicaSimples.Teste2;
 
 public class main {
+    public static void main(String[] args) {
+
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < numeros.length; i++) {
+
+            if (numeros[i] % 2 == 0) {
+                System.out.println(numeros[i]);
+            }
+        }
+    }
 }
