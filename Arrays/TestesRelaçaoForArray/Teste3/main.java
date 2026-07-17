261b5ba0510c454f9b47d23f56c1dcae0d2223b7
matheussantos4 <matheus.santos88y@gmail.com>
2026-07-17 18:42:45 -0300

    feat: testes trabalhando lógica de arrays.

---
+++ Arrays/TestesRelaçaoForArray/Teste3/main.java
@@ -0,0 +1,16 @@
+package Arrays.TestesRelaçaoForArray.Teste3;
+
+public class main {
+    public static void main(String[] args) {
+
+        int[] listaDeNum = {1, 3, 6, 8, 2, 9, 4, 7, 10, 5};
+
+        for (int i = 0; i < 10; i++) {
+
+            if (listaDeNum[i] > 5) {
+
+                System.out.println(listaDeNum[i]);
+            }
+        }
+    }
+}
