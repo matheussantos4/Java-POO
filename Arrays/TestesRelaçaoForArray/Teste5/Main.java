696131745d9094085deec6fb7e96232120f35ec2
matheussantos4 <matheus.santos88y@gmail.com>
2026-07-19 20:54:47 -0300

    Doc: descrição lógica funcionamento teste 5.

--- Arrays/TestesRelaçaoForArray/Teste5/Main.java
+++ Arrays/TestesRelaçaoForArray/Teste5/Main.java
@@ -4,14 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         int[] numeros = {4, 12, 7, 9, 25, 3, 18};
+        //Array de numeros inteiros.
 
         int maior = numeros[0];
+        //variável que vai comparar os elementos do array.
 
-        for (int i = 0; i < numeros.length; i++) {
-
+        for (int i = 0; i < numeros.length; i++)
+        //simples contador para ir passando os elementos do array
+        {
             if (numeros[i] > maior) {
                 maior = numeros[i];
                 System.out.println(maior);
+
+                //Lógica : se o elemento do array for maior que o anterior
+                // iguale com a variável maior e a Printe.
             }
         }
     }
