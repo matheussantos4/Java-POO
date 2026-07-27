8e390390a3116fcbafd41d400bbebe52b5ad26d9
matheussantos4 <matheus.santos88y@gmail.com>
2026-07-27 20:56:29 -0300

    Chore: Testando conhecimentos ArrayList

---
+++ Arrays/ArrayList/Main.java
@@ -0,0 +1,25 @@
+package Arrays.ArrayList;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Testando conhecimento ArrayList.
+
+        ArrayList<String> nomes = new ArrayList<>();
+
+        nomes.add("Matheus");
+        nomes.add("joao");
+        nomes.add("Maria");
+
+        for (int i = 0; i < nomes.size(); i++) {
+            System.out.println(nomes.get(i));
+        }
+
+        nomes.remove(0);
+
+        System.out.println(nomes.getFirst());
+        System.out.println(nomes.getLast());
+    }
+}
