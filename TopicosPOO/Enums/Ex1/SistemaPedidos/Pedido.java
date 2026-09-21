6bca8a5cc8da6c2093d75b444f221ca5c6604657
matheussantos4 <matheus.santos88y@gmail.com>
2026-09-21 18:32:50 -0300

    feat: desenvolvimento classe pedido.

--- TopicosPOO/Enums/Ex1/SistemaPedidos/Pedido.java
+++ TopicosPOO/Enums/Ex1/SistemaPedidos/Pedido.java
@@ -5,4 +5,14 @@ import java.util.ArrayList;
 public class Pedido {
 
     private ArrayList<ItemPedido> itens = new ArrayList<>();
+    private int contador, ID, mesa;
+
+    public void adicionarItem(ItemPedido ItemPedido) {
+        itens.add(ItemPedido);
+    }
+
+    public Pedido(int ID, int mesa) {
+        this.ID = ID;
+        this.mesa = mesa;
+    }
 }
