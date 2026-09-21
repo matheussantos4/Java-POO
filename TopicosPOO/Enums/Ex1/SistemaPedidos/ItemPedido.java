182152ae0df21c2f5dae86410b7c5af45c678a4e
matheussantos4 <matheus.santos88y@gmail.com>
2026-09-21 18:20:52 -0300

    chore: Criação classe ItemPedido.

---
+++ TopicosPOO/Enums/Ex1/SistemaPedidos/ItemPedido.java
@@ -0,0 +1,37 @@
+package TopicosPOO.Enums.Ex1.SistemaPedidos;
+
+public class ItemPedido {
+    private String nome;
+    private int Quantd;
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantd() {
+        return Quantd;
+    }
+
+    public void setQuantd(int quantd) {
+        Quantd = quantd;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public ItemPedido(String nome, int Quantd, double preco) {
+        this.nome = nome;
+        this.Quantd = Quantd;
+        this.preco = preco;
+    }
+}
