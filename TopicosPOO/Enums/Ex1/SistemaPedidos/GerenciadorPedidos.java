4054f637870d4b55ec74147079322efaee38f81e
matheussantos4 <matheus.santos88y@gmail.com>
2026-09-21 18:20:20 -0300

    chore: Criação Gerenciador de pedidos.

---
+++ TopicosPOO/Enums/Ex1/SistemaPedidos/GerenciadorPedidos.java
@@ -0,0 +1,10 @@
+package TopicosPOO.Enums.Ex1.SistemaPedidos;
+
+import java.util.ArrayList;
+
+public class GerenciadorPedidos {
+
+    private ArrayList<Pedido> pedidos = new ArrayList<>();
+    private int contador = 0;
+
+}
