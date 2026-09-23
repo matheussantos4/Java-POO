29411ec19ec38a6d46c1d2f496fd5935edb8d472
matheussantos4 <matheus.santos88y@gmail.com>
2026-09-22 22:16:26 -0300

    feat: Testes de funcionamentos da main.

--- TopicosPOO/Enums/Ex1/SistemaPedidos/Main.java
+++ TopicosPOO/Enums/Ex1/SistemaPedidos/Main.java
@@ -6,6 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        Pedido n1 = new Pedido(1, 12);
+        int id = sc.nextInt();
+        int mesa = sc.nextInt();
+        Pedido n2 = new Pedido(id, mesa);
         System.out.println(StatusPedido.PRONTO.tempoEstimado());
+        
+
     }
 }
