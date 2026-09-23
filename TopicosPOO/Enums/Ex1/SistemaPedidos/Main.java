62f23f728f67f27d6f03f9f0872b24b89097082a
matheussantos4 <matheus.santos88y@gmail.com>
2026-09-23 18:27:10 -0300

    feat: Desenvolvimento menu do usuário.

--- TopicosPOO/Enums/Ex1/SistemaPedidos/Main.java
+++ TopicosPOO/Enums/Ex1/SistemaPedidos/Main.java
@@ -6,12 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        Pedido n1 = new Pedido(1, 12);
-        int id = sc.nextInt();
-        int mesa = sc.nextInt();
-        Pedido n2 = new Pedido(id, mesa);
-        System.out.println(StatusPedido.PRONTO.tempoEstimado());
-        
+        System.out.println("------- Gerenciamento Pedidos -------");
+        System.out.print("Selecione uma opção: ");
+        System.out.print("(1) Adicionar pedidos");
+        System.out.print("(2) Listar pedidos");
+        System.out.print("(3) Remover pedidos");
+        System.out.print("(4) Modificar pedidos");
 
     }
 }
